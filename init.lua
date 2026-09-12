fecb4d5d0bfb9a016ad6fa82cc1570d7a0f323d3
FigureOffic <artemtabacnikov2@gmail.com>
2026-09-12 15:56:00 +0300

    init.lua

--- init.lua
+++ init.lua
@@ -3,5 +3,7 @@ _G.PromtMZ = _G.PromtMZ or {}
 local base = "https://raw.githubusercontent.com/FigureOffic/PromtMZ-Scripts/main/"
 
 loadstring(game:HttpGet(base .. "main.lua"))()
-task.wait(0.5)
+task.wait(0.3)
+loadstring(game:HttpGet(base .. "functions.lua"))()
+task.wait(0.3)
 loadstring(game:HttpGet(base .. "visuals.lua"))()

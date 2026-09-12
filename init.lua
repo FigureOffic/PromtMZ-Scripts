0ee64d76e1eb30cf7e5eedf4cebbca7081159e75
FigureOffic <artemtabacnikov2@gmail.com>
2026-09-12 20:32:04 +0300

    init.lua

--- init.lua
+++ init.lua
@@ -3,12 +3,12 @@ _G.PromtMZ = _G.PromtMZ or {}
 local base = "https://raw.githubusercontent.com/FigureOffic/PromtMZ-Scripts/main/"
 
 local m = loadstring(game:HttpGet(base .. "main.lua"))
-if m then m() else warn("main.lua не скомпилировался") end
+if m then m() end
 task.wait(1)
 
 local f = loadstring(game:HttpGet(base .. "functions.lua"))
-if f then f() else warn("functions.lua не скомпилировался") end
+if f then f() end
 task.wait(0.5)
 
 local v = loadstring(game:HttpGet(base .. "visuals.lua"))
-if v then v() else warn("visuals.lua не скомпилировался") end
+if v then v() end

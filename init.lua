07dd6604cb6373268514ca82f4c8be5cfe82ab8d
FigureOffic <artemtabacnikov2@gmail.com>
2026-09-12 20:28:25 +0300

    init.lua

--- init.lua
+++ init.lua
@@ -1,17 +1,9 @@
 -- init.lua
-if _G.PromtMZ and _G.PromtMZ.loaded then
-    warn("[PromtMZ] Уже загружен")
-    return
-end
-
 _G.PromtMZ = _G.PromtMZ or {}
-_G.PromtMZ.loaded = true
-
 local base = "https://raw.githubusercontent.com/FigureOffic/PromtMZ-Scripts/main/"
-local salt = "?t=" .. tostring(tick())
 
-loadstring(game:HttpGet(base .. "main.lua" .. salt))()
+loadstring(game:HttpGet(base .. "main.lua"))()
 task.wait(1)
-loadstring(game:HttpGet(base .. "functions.lua" .. salt))()
+loadstring(game:HttpGet(base .. "functions.lua"))()
 task.wait(0.5)
-loadstring(game:HttpGet(base .. "visuals.lua" .. salt))()
+loadstring(game:HttpGet(base .. "visuals.lua"))()
